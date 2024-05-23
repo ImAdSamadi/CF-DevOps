a3e0c190deb4451be26ef88bba953d70e6f594ae
ImAdSamadi <imad.samadi@etu.uae.ac.ma>
2024-05-23 14:47:36 +0100

    Adding fname to class Etudiant.java

--- src/main/java/org/example/Etudiant.java
+++ src/main/java/org/example/Etudiant.java
@@ -5,8 +5,13 @@ public class Etudiant {
     public int id;
     public String name;
 
+    public String fname;
+
     public Etudiant(int id, String name) {
         this.id = id;
         this.name = name;
     }
+
+
+
 }
